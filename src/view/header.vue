6cdea9d5eb367b59dba5ce7daeec2ad92952d054
syc9527 <1142797508@qq.com>
2023-03-18 10:48:59 +0800

    新增头部导航栏

---
+++ src/view/header.vue
@@ -0,0 +1,37 @@
+<template>
+  <!-- 主要结构框架 -->
+  <div class="header-box">
+    <div class="header-container">
+          <div>首页</div>
+          <div>博客</div>
+          <div>留言</div>
+          <div>友链</div>
+   </div>
+</div>
+        
+</template>
+
+<script setup>
+const list = [
+    {
+    name: '首页',
+  },
+  {
+    name: '博客', 
+  },
+  {
+    name: '友链', 
+  },
+  {
+    name: '留言'
+    }
+  ]
+</script>
+
+<style lang="scss" scoped>
+.header-box{
+  .header-containerr {
+  }
+}
+ 
+</style>
